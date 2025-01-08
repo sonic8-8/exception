f3034f6bb7dd251267f1f86b3ef0c747faebb83a
이승환 <seaung13@gmail.com>
2025-01-08 16:40:07 +0900

    feat: 서블릿 예외 처리 - 2가지 방식 (exception, response.sendError())

---
+++ src/main/java/com/chain/exception/servlet/ServletExceptionController.java
@@ -0,0 +1,28 @@
+package com.chain.exception.servlet;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ServletExceptionController {
+
+    @GetMapping("/error-ex")
+    public void errorException() {
+        throw new RuntimeException("예외 발생!");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+}
