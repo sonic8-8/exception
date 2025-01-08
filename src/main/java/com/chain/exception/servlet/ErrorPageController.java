a5daa7671d52f0a42c36c30883d361ec5e209548
이승환 <seaung13@gmail.com>
2025-01-08 17:25:33 +0900

    feat: 서블릿 예외 처리 - 필터

--- src/main/java/com/chain/exception/servlet/ErrorPageController.java
+++ src/main/java/com/chain/exception/servlet/ErrorPageController.java
@@ -38,7 +38,7 @@ public class ErrorPageController {
         log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
         log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
         log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
-        log.info("dispatchType={}", request.getDispatcherType());
+        log.info("DispatcherType={}", request.getDispatcherType());
     }
 
 }
