784c04481a860d127a4c5db641c7bf823616da80
이승환 <seaung13@gmail.com>
2025-01-08 20:52:16 +0900

    feat: API 예외 처리 - MyHandlerExceptionResolver 추가

--- src/main/java/com/chain/exception/WebConfig.java
+++ src/main/java/com/chain/exception/WebConfig.java
@@ -1,13 +1,17 @@
 package com.chain.exception;
 
+import com.chain.exception.resolver.MyHandlerExceptionResolver;
 import com.chain.exception.servlet.filter.LogFilter;
 import com.chain.exception.servlet.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -28,5 +32,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
     }
 }
